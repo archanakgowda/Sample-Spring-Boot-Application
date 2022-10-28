12f270296d2e06fa6f305dc714ce9ff34e73a079
archanakgowda <archanak822000@gmail.com>
2022-10-28 16:07:15 +0530

    initial commit to sample application

--- src/main/java/com/example/sample/controller/SampleController.java
+++ src/main/java/com/example/sample/controller/SampleController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class SampleController {
+public class SampleController { //test to push
 
     @GetMapping("/data")
     public String getSampleData(){
